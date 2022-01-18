b081d70f867847cf31f004a64bd3ffc7c3ac5895
Ahn729 <58076062+Ahn729@users.noreply.github.com>
2022-01-18 16:32:44 +0100

    Improve autoML

--- auto_ml/cv_model_comparison.py
+++ auto_ml/cv_model_comparison.py
@@ -6,6 +6,7 @@ from data_exploration.feature_selection import obtain_independent_variables
 
 from model_selection.cv_model_comparison import ModelComparer
 
+
 class AutoMLModelComparer:
     """Performs a k-fold cross validation to compare multiple models
 
@@ -36,13 +37,12 @@ class AutoMLModelComparer:
     def result(self):
         return self._result
 
-
-    def create_comparison_result(self, target):
+    def create_comparison_result(self, target, **kwargs):
         self._reduced_data = reduce_dataset(self.data)
         self._outliers = recursive_outlier_detection(self.reduced_data)
         df = self.reduced_data.drop(self.outliers.index)
         X, y = df.drop(columns=target), df[target]
         self._ind_vars = obtain_independent_variables(X)
         X = X[self.ind_vars]
-        self._result = ModelComparer(StandardScaler()).fit(X, y)
+        self._result = ModelComparer(StandardScaler(), **kwargs).fit(X, y)
         return self.result

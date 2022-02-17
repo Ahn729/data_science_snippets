a9265aeb8afd864f781fd2b2388f9faa513cd924
Ahn729 <58076062+Ahn729@users.noreply.github.com>
2022-02-17 16:50:40 +0100

    Minor changes and fixes in Kruskal

--- data_exploration/categorical_vs_numeric.py
+++ data_exploration/categorical_vs_numeric.py
@@ -1,4 +1,8 @@
 import warnings
+from typing import Optional, Tuple
+
+import matplotlib
+from pandas.core.dtypes.common import is_numeric_dtype
 from statsmodels.api import stats
 from statsmodels.formula.api import ols
 import numpy as np
@@ -10,7 +14,7 @@ import matplotlib.pyplot as plt
 __all__ = ['anova', 'anova_for_all', 'kruskal', 'kruskal_for_all', 'kruskal_one_vs_all', 'strip_and_boxplot']
 
 
-def anova(dataset: pd.DataFrame, test_col: str, target_col: str):
+def anova(dataset: pd.DataFrame, test_col: str, target_col: str) -> np.float:
     """Performs a one-way ANOVA F-test for groups in test_col with values in target_col.
 
     Note that ANOVA tests reqire independently, normally distributed samples with
@@ -49,18 +53,21 @@ def anova_for_all(dataset: pd.DataFrame, target_col: str, significance: float = 
     """
     result_dict = {}
     col_names = dataset.select_dtypes(object).columns
+
     for col in col_names:
         try:
             pr_f = anova(dataset, col, target_col)
             if pr_f <= significance:
                 result_dict[col] = pr_f
-        except:
-            print(f'Error evaluating column {col}')
+        except Exception as e:
+            print(f'Error evaluating column {col}: {e}')
+
     df = pd.DataFrame(data=result_dict.items(), columns=['Column', 'p-val'])
+    df['Bonf_p'] = df['p-val'] * len(df)
     return df.set_index('Column').sort_values(by='p-val')
 
 
-def kruskal(dataset: pd.DataFrame, test_col: str, target_col: str, nan_policy: str = 'propagate'):
+def kruskal(dataset: pd.DataFrame, test_col: str, target_col: str, nan_policy: str = 'propagate') -> np.float:
     """Applies Kruskal-Wallis H-test to a single column
 
 
@@ -82,6 +89,7 @@ def kruskal(dataset: pd.DataFrame, test_col: str, target_col: str, nan_policy: s
     column = dataset[test_col]
     if nan_policy == 'handle' and column.dtype.name != 'category':
         column = column.fillna('__n_o_n_e__')
+
     # From scipi.stats.kruskal:
     # Due to the assumption that H has a chi square distribution, the number of
     # samples in each group must not be too small. A typical rule is that each
@@ -92,24 +100,31 @@ def kruskal(dataset: pd.DataFrame, test_col: str, target_col: str, nan_policy: s
     if len(dataset) / 5 < column.nunique():
         warnings.warn(f'Ignoring column {test_col}: Too few (<5) samples in each class.')
         return np.nan
+
     samples = [dataset[column == value][target_col] for value in column.unique() if not pd.isna(value)]
     _nan_policy = nan_policy if nan_policy != 'handle' else 'omit'
-    return sp.kruskal(*samples, nan_policy=_nan_policy).pvalue
+    p_value = sp.kruskal(*samples, nan_policy=_nan_policy).pvalue
+    if np.isnan(p_value):
+        warnings.warn(f"Obtained nan for column {test_col}. This may happen if your input contained "
+                      f"nan values. In that case, consider setting nan_policy='handle'.")
+    return p_value
 
 
 def kruskal_for_all(dataset: pd.DataFrame,
                     target_col: str,
                     significance: float = 1,
                     nan_policy: str = 'propagate') -> pd.DataFrame:
-    """Applies Kruskal-Wallis H-test to all categorical columns
+    """Applies Kruskal-Wallis H-test to all columns
 
     Applies Kruskal-Wallis H-test to all tuples of the form
-    (categorical col, target_col) in order to test whether the medians in each
-    of the classes are equal.
+    (col, target_col) in order to test whether the medians in each
+    of the classes are equal. If target_col is numeric, kruskal
+    checks categorical columns and vice versa.
 
     Args:
         dataset: dataset to check
-        target_col: numerical column to check categorical columns against
+        target_col: numerical column to check categorical columns against or
+            categorical column to check numerical columns against
         significance: If set, only return values with p-value <= significance
         nan_policy: One of {'handle', 'omit', 'propagate', 'raise'}.
             'handle' removes nan values in categorical columns and treats them
@@ -120,15 +135,29 @@ def kruskal_for_all(dataset: pd.DataFrame,
     """
 
     result_dict = {}
-    col_names = dataset.select_dtypes([object, 'datetime', 'category']).columns
+    if num_vs_cat_mode := is_numeric_dtype(dataset[target_col]):
+        col_names = dataset.select_dtypes([object, 'datetime', 'category']).columns
+    else:
+        col_names = dataset.select_dtypes(np.number).columns
+
     for col in col_names:
-        pr_f = kruskal(dataset, col, target_col, nan_policy=nan_policy)
+        try:
+            if num_vs_cat_mode:
+                pr_f = kruskal(dataset, col, target_col, nan_policy=nan_policy)
+            else:
+                pr_f = kruskal(dataset, target_col, col, nan_policy=nan_policy)
+        except ValueError as e:
+            warnings.warn(str(e))
+            pr_f = 1
         if not significance or pr_f <= significance:
             result_dict[col] = [pr_f, dataset[col].nunique(dropna=(nan_policy != 'handle'))]
+
+    result_col_name = f'p({target_col})'
     df = pd.DataFrame.from_dict(
-        result_dict, orient='index', columns=[f'p({target_col})', 'nunique']
-    )
-    return df.sort_values(by=f'p({target_col})').astype({f'p({target_col})': float, 'nunique': int})
+        result_dict, orient='index', columns=[result_col_name, 'nunique']
+    ).astype({result_col_name: float, 'nunique': int})
+    df[f'Bonf_{result_col_name}'] = df[result_col_name] * len(df)
+    return df.sort_values(by=result_col_name)
 
 
 def kruskal_one_vs_all(dataset: pd.DataFrame,
@@ -158,6 +187,7 @@ def kruskal_one_vs_all(dataset: pd.DataFrame,
     result_dict = dict()
     categories = dataset[cat_col].unique()
     num_cat = len(categories)
+
     for category in categories:
         in_cat = dataset[dataset[cat_col] == category][target_col]
         nin_cat = dataset[dataset[cat_col] != category][target_col]
@@ -177,7 +207,15 @@ def kruskal_one_vs_all(dataset: pd.DataFrame,
     return df.sort_values(by='p')
 
 
-def strip_and_boxplot(data, x, y, hue=None, figsize=(12, 8), alpha=1, ax=None, strip_kwargs=None, box_kwargs=None):
+def strip_and_boxplot(data: pd.DataFrame,
+                      x: str,
+                      y: str,
+                      hue: Optional[str] = None,
+                      figsize: Tuple[int, int] = (12, 8),
+                      alpha: float = 1,
+                      ax: Optional[matplotlib.axes] = None,
+                      strip_kwargs: Optional[dict] = None,
+                      box_kwargs: Optional[dict] = None) -> None:
     strip_kwargs, box_kwargs = strip_kwargs or dict(), box_kwargs or dict()
     if not ax:
         fig, ax = plt.subplots(figsize=figsize)
